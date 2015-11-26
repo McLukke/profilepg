f3ee59b2fb50dd4c35473dc8844e359112364a67
Luke McKennan <luke.mckennan@gmail.com>
2015-11-26 17:25:41 +0800

    cleaning up files adding background / foreground / mobile background add logo add mobile title add mobile text add desktop rotating titles add desktop rotating texts

---
+++ main.php
@@ -0,0 +1,32 @@
+<html>
+<?php include('partials/head.html'); ?>
+<body>
+	<main id="landingPage">
+		<div class="mobileBG"></div>
+
+		<div class="parallaxBG">
+			<img class="foreground" src="images/voilah-teaser-pLayer-z1@1x.png" />
+			<img class="background" src="images/voilah-teaser-pLayer-z2@1x.png" />
+		</div>
+
+		<div id="logo"></div>
+
+		<div id="content">
+			<h1 id="mobile-title">Hello There!</h1>
+			<p id="mobile-text">Hey, this is where I introduce myself and talk about what I do...</p>
+
+			<ul class="title">
+				<li>Hello There!</li>
+				<li>Check out cool stuff</li>
+			</ul>
+
+			<ul class="text">
+				<li>Hey, this is where I introduce myself and talk about what I do...</li>
+				<li>Move the mouse to the browser edges to see more!</li>
+			</ul>
+
+			<div class="contact"></div>
+		</div>
+	</main>
+</body>
+</html>
