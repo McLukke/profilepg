d6dfb6f2814c4af770910f55aa3a213e0e66abc6
Luke McKennan <luke.mckennan@gmail.com>
2015-12-23 18:02:06 +0800

    [Contact] new icons, new Google+ badge, change double to single mid-line | separated files for [About Me] and [Contact]

--- index.php
+++ index.php
@@ -42,28 +42,27 @@
 				</div>
 
 				<div id="contactInfo" class="popups">
-					<h1>Feel free to contact me</h1>
+					<h1>Feel free to contact me:</h1>
 					<a href="mailto:kenny@whynotkenny.com?subject=Kenny, I'm contacting you directly from your website!">
 						<p>Email: Kenny@whynotkenny.com</p>
 					</a>
-					<section>
-						<span>OR...</span>
-					</section>
-<!--						<h1>Connect with me</h1>
- 						<span>
-							<a href="http://ca.linkedin.com/in/kenshengjialu"><img src="images/linkedin.png" alt="LinkedIn" /></a>
-							<a href="http://github.com/McLukke"><img src="images/github.png" alt="Github" /></a>
-							<div id="google_spacing"><div class="g-follow" data-href="https://plus.google.com/106759043569228951176" data-rel="author" data-height="24"></div></div>
-						</span> -->
 
-<!-- 					<section>
-						<hr />
-						OR
-						<hr />
-					</section>
+					<section><span>OR...</span></section>
+
+					<h1>Connect with me:</h1>
+					<span>
+						<a href="http://ca.linkedin.com/in/kenshengjialu"><img src="images/linkedin.png" alt="LinkedIn" /></a>
+						<a href="http://github.com/McLukke"><img src="images/github.png" alt="Github" /></a>
+						<div id="google_spacing"><div class="g-follow" data-href="https://plus.google.com/106759043569228951176" data-rel="author" data-height="24"></div></div>
+					</span> 
+
+					<section><span>OR...</span></section>
+
 					<h1>Give me you feedback on this site!</h1>
-					<a href="#"><img src="images/like.png" alt="Like" /></a>
-					<a href="#"><img src="images/dislike.png" alt="Dislike" /></a> -->
+					<div class="voting">
+						<a href="#"><img src="images/like.png" alt="Like" /><span>Like</span></a>
+						<a href="#"><img src="images/dislike.png" alt="Dislike" /><span>Dislike</span></a>
+					</div>
 				</div>
 
 				<div id="leftHover" class="triggers"></div>
