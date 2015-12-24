bcb393296f427a41007146c2cbe145e44638ab76
Luke McKennan <luke.mckennan@gmail.com>
2015-12-24 10:45:05 +0800

    testing bg overflow

---
+++ partials/content.php
@@ -0,0 +1,60 @@
+<div class="content">
+	<div class="content__text-wrapper">
+	 	<div id="text1" class="text">
+	 		<h1>Hello There!</h1>
+	 		<p>I'm Kenny and thank you for visiting my site!</p>
+	 	</div>
+	 	<div id="text2" class="text">
+	 		<h1>I'm a web developer</h1>
+	 		<p>That dabbles in web magic :D</p>
+	 	</div>
+	 	<div id="text3" class="text">
+	 		<h1>"Who do you work for?"</h1>
+	 		<p>I want to work with the best, the engaged and the sensible to help make the Internet a better place</p>
+	 	</div>
+	 	<div id="text4" class="text">
+	 		<h1>When I'm not coding with the coolest?</h1>
+	 		<p>You'll find me studying rocks, riding my bike or getting yelled at on the Internet =(</p>
+	 	</div>
+	</div>
+
+	<div class="hover-effects">
+		<div id="aboutMe" class="popups text-center">
+			<h1>Here's a little bit about me:</h1>
+			<br />
+			<p>Coming Soon!</p>
+		</div>
+
+		<div id="contactInfo" class="popups">
+			<h1>Feel free to contact me:</h1>
+			<a href="mailto:kenny@whynotkenny.com?subject=Kenny, I'm contacting you directly from your website!">
+				<p>Email: Kenny@whynotkenny.com</p>
+			</a>
+
+			<section><span>OR...</span></section>
+
+			<h1>Connect with me:</h1>
+			<span>
+				<a href="http://ca.linkedin.com/in/kenshengjialu"><img src="images/linkedin.png" alt="LinkedIn" /></a>
+				<a href="http://github.com/McLukke"><img src="images/github.png" alt="Github" /></a>
+				<div id="google_spacing"><div class="g-follow" data-href="https://plus.google.com/106759043569228951176" data-rel="author" data-height="24"></div></div>
+			</span> 
+
+			<section><span>OR...</span></section>
+
+			<h1>Feedback Section Coming soon!</h1>
+
+			<!-- <h1>Give me you feedback on this site!</h1>
+			<div class="voting">
+				<a href="#"><img src="images/like.png" alt="Like" /><span>Like</span></a>
+				<a href="#"><img src="images/dislike.png" alt="Dislike" /><span>Dislike</span></a>
+			</div> -->
+		</div>
+
+		<div id="leftHover" class="triggers"></div>
+		<div id="topHover" class="triggers"></div>
+		<div id="rightHover" class="triggers"></div>
+		<div id="bottomHover" class="triggers"></div>
+	</div>
+
+</div>
