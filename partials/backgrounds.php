46443f3803492b0370d4f1f23958f35578309d0d
Kennington <ken@defyent.com>
2016-01-04 11:56:14 +0800

    new branch for slider

--- partials/backgrounds.php
+++ partials/backgrounds.php
@@ -1,8 +1,12 @@
 <div class="mobileBG"></div>
 
 <div class="parallaxWrapper">
-	<div class="parallaxBG">
+<!-- 	<div class="parallaxBG">
 		<img id="foreground" class="desktop-bg" src="images/voilah-teaser-pLayer-z2@1x.png" />
 		<img id="background" class="desktop-bg" src="images/kenny_bg.jpg" />
-	</div>
+	</div> -->
 </div>
+
+<div class="projects project1"></div>
+<div class="projects project2"></div>
+<div class="projects project3"></div>
