a5e031c7eddb8ad589ff1f8cf085b9f5e4d89644
Kennington <ken@defyent.com>
2016-01-04 17:08:54 +0800

    new nav arrows logic

--- dist/all.js
+++ dist/all.js
@@ -64,68 +64,9 @@ function initBackground(halfWidth, halfHeight, maxRotationX, maxRotationY, aspec
 
 // js goes here
 $(function() {
-  $('#fullpage').fullpage({
-    //Navigation
-    menu: '#menu',
-    lockAnchors: false,
-    anchors:['firstPage', 'secondPage'],
-    navigation: false,
-    navigationPosition: 'right',
-    navigationTooltips: ['firstSlide', 'secondSlide'],
-    showActiveTooltip: false,
-    slidesNavigation: false,
-    slidesNavPosition: 'bottom',
-
-    //Scrolling
-    css3: true,
-    scrollingSpeed: 700,
-    autoScrolling: true,
-    fitToSection: true,
-    fitToSectionDelay: 1000,
-    scrollBar: false,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: true,
-    continuousVertical: false,
-    normalScrollElements: '#element1, .element2',
-    scrollOverflow: true,
-    touchSensitivity: 15,
-    normalScrollElementTouchThreshold: 5,
-
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-
-    //Design
-    controlArrows: true,
-    verticalCentered: true,
-    resize : false,
-    sectionsColor : ['#fff', '#000'],
-    paddingTop: '0',
-    paddingBottom: '0',
-    fixedElements: '#header, .footer',
-    responsiveWidth: 0,
-    responsiveHeight: 0,
-
-    //Custom selectors
-    sectionSelector: '.section',
-    slideSelector: '.slide',
-
-    //events
-    onLeave: function(index, nextIndex, direction){},
-    afterLoad: function(anchorLink, index){},
-    afterRender: function(){},
-    afterResize: function(){},
-    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-  });
-
 	// get css 'content' value + remove extra quotes
   var mediaQuery = window.getComputedStyle(document.querySelector('.parallaxWrapper'), ':before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
-  
+
   // define initial variables
   var halfHeight = $(window).height() / 2,
       halfWidth = $(window).width() / 2;
@@ -193,6 +134,66 @@ $(function() {
     }
     $('#text' + textIndex).delay(fadeSpeed).fadeIn(fadeSpeed);
   }, interval);
+});
+$(function() {
+  $('#fullpage').fullpage({
+    //Navigation
+    menu: '#menu',
+    lockAnchors: false,
+    anchors:['firstPage', 'secondPage'],
+    navigation: false,
+    navigationPosition: 'right',
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+    showActiveTooltip: false,
+    slidesNavigation: false,
+    slidesNavPosition: 'bottom',
+
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: true,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
+
+    //Accessibility
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+
+    //Design
+    controlArrows: true,
+    verticalCentered: true,
+    resize : false,
+    sectionsColor : ['#fff', '#000'],
+    paddingTop: '0',
+    paddingBottom: '0',
+    fixedElements: '#header, .footer',
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+
+    //Custom selectors
+    sectionSelector: '.section',
+    slideSelector: '.slide',
+
+    //events
+    onLeave: function(index, nextIndex, direction){},
+    afterLoad: function(anchorLink, index){},
+    afterRender: function(){},
+    afterResize: function(){},
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+  });
 });
 $(function() {
   // hover & click effects
