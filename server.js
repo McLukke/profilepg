0d01b3a81c12c323020d527edb9fc7d7dd50ac0c
Kenny Lu <Kenny@whynotkenny.com>
2017-01-07 14:12:45 -0500

    Webpack setup install new packages

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  stats: 'errors-only'
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(3000, function(err) {
+const PORT = 3000;
+
+app.listen(PORT, function(err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log(`Listening at http://localhost:${PORT}/`);
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,55 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-regenerator-runtime',
+    'react-hot-loader/patch',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: [
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 10',
+        'Edge >= 1',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6',
+      ],
+    }),
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader/webpack', 'babel'],
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css?modules&localIdentName=[path]---[local]!postcss!resolve-url!sass?sourceMap',
+      },
+      {
+        test: /\.less$/,
+        loaders: ['style', 'css', 'less'],
+      },
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
