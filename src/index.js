31c7339104d847b8b56941c6aa9b36f827341bc3
Kenny Lu <Kenny@whynotkenny.com>
2017-01-07 14:29:00 -0500

    Setup store and initial app structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './global/styles';
+import reducers from './reducers';
+import routes from './routes';
+
+export const store = createStore( // eslint-disable-line
+  reducers,
+  compose(
+    applyMiddleware(),
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+  ),
+);
+
+// HMR
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router key={new Date()} history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('root'),
+);
