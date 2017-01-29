fd639e46b80bdb05221b18351beabe92bf7d966f
Kenny Lu <Kenny@whynotkenny.com>
2017-01-28 23:47:23 -0500

    Skills section complete

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -1,3 +1,22 @@
+export const mySkills = [
+  {
+    name: 'HTML5',
+    percentage: 81,
+  },
+  {
+    name: 'CSS3',
+    percentage: 87,
+  },
+  {
+    name: 'Javascript',
+    percentage: 94,
+  },
+  {
+    name: 'PHP',
+    percentage: 67,
+  },
+];
+
 export const sections = [
   {
     name: 'landingImage',
--- src/home/components/skills/index.js
+++ src/home/components/skills/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Scroll from 'react-scroll';
-import { sections } from 'constants';
+import { mySkills, sections } from 'constants';
+import variables from 'variables';
+import { Circle } from 'rc-progress';
 
-import styles from '../content/styles.scss';
+import styles from './styles.scss';
 
 const Skills = () =>
-  <Scroll.Element name={sections[2].name} className={styles.base}>
-    {sections[2].title} Goes Here
+  <Scroll.Element name={sections[1].name} className={styles.base}>
+    {mySkills.map((skill, index) =>
+      <div key={index} className={styles['progress-bar-wrapper']}>
+        <Circle
+          className={styles['progress-bar']}
+          strokeWidth="12"
+          trailWidth="12"
+          percent={skill.percentage}
+          strokeLinecap="square"
+          strokeColor={variables.aqua}
+        />
+        <span className={styles['skill-title']}>
+          {skill.name}
+        </span>
+      </div>,
+    )}
   </Scroll.Element>;
 
 export default Skills;
