874b32750d07eebaffd96b02df9f7b207431c9fa
Kenny Lu <Kenny@whynotkenny.com>
2017-02-08 22:48:02 -0500

    Add portfolio images / delete old

--- src/home/components/portfolio/index.js
+++ src/home/components/portfolio/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Scroll from 'react-scroll';
 import { sections } from 'constants';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 
 import styles from '../content/styles.scss';
 
 const Portfolio = () =>
   <Scroll.Element name={sections[3].name} className={styles.base}>
-    {sections[3].title} Goes Here
+    <Row>
+      <Col
+        xs={12}
+        md={10}
+        mdOffset={1}
+        lg={8}
+        lgOffset={2}
+      >
+        <Row>
+          <Col xs={4}>
+          </Col>
+        </Row>
+
+
+        <Row>
+          <Col xs={4}>
+          </Col>
+        </Row>
+
+
+        <Row>
+          <Col xs={4}>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   </Scroll.Element>;
 
 export default Portfolio;
