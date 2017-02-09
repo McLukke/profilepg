fcddee83960e0a90b667e7486ec7dd1253e03b00
Kenny Lu <Kenny@whynotkenny.com>
2017-02-08 21:49:19 -0500

    Revert "Make header fixed"
    This reverts commit 7cc78981417fe08421163503f33d96021616215a.

--- src/home/components/portfolio/index.js
+++ src/home/components/portfolio/index.js
@@ -1,42 +1,12 @@
 import React from 'react';
 import Scroll from 'react-scroll';
 import { sections } from 'constants';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 
 import styles from '../content/styles.scss';
 
 const Portfolio = () =>
   <Scroll.Element name={sections[3].name} className={styles.base}>
-    <Row>
-      <Col
-        xs={12}
-        md={10}
-        mdOffset={1}
-        lg={8}
-        lgOffset={2}
-      >
-        <Row>
-          <Col>
-
-          </Col>
-        </Row>
-
-
-        <Row>
-          <Col>
-
-          </Col>
-        </Row>
-
-
-        <Row>
-          <Col>
-            
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    {sections[3].title} Goes Here
   </Scroll.Element>;
 
 export default Portfolio;
