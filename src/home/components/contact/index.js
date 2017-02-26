786efbdd6b4cfe8116bb99173c660bea8b1567d3
Kenny Lu <Kenny@whynotkenny.com>
2017-02-26 16:35:08 -0500

    Remove redundant styles

--- src/home/components/contact/index.js
+++ src/home/components/contact/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Scroll from 'react-scroll';
 import { sections } from 'content';
 
-import styles from '../content/styles.scss';
-
 const Contact = () =>
-  <Scroll.Element name={sections[4].name} className={styles.base}>
+  <Scroll.Element name={sections[4].name}>
     {sections[4].title} Goes Here
   </Scroll.Element>;
 
