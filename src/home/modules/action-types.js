f897c1615dc38f8fbc3ad7383184ca1eb5f3eac1
Kenny Lu <Kenny@whynotkenny.com>
2017-02-26 16:31:26 -0500

    Create redux action-types

---
+++ src/home/modules/action-types.js
@@ -0,0 +1,7 @@
+const profilePageBase = 'PROFILE-PAGE-BASE';
+
+const actionTypes = {
+  toggleHeaderBg: `${profilePageBase}/TOGGLE_HEADER_BG`,
+};
+
+export default actionTypes;
---
+++ src/home/modules/index.js
@@ -0,0 +1,30 @@
+import { combineReducers } from 'react-redux';
+
+import actionTypes from './action-types';
+
+
+export const toggleHeaderBg = trigger => ({
+  type: actionTypes.toggleHeaderBg,
+  payload: trigger,
+});
+
+
+const homePageReducer = (
+  state = {
+    showBg: false,
+  },
+  action,
+) => {
+  switch (action.type) {
+    case actionTypes.toggleHeaderBg:
+      return {
+        ...state,
+        showBg: action.payload,
+      };
+
+    default:
+      return { ...state };
+  }
+};
+
+export default homePageReducer;
