93ffee505811f045c28b77066f7dce4475a3b58c
Kenny Lu <Kenny@whynotkenny.com>
2017-03-10 21:54:38 -0500

    separate ProfileImages to have react state

--- src/home/components/portfolio/index.js
+++ src/home/components/portfolio/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import Scroll from 'react-scroll';
+import cx from 'classnames';
 import { sections, portfolioImages } from 'content';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
@@ -9,36 +10,67 @@ import TextBlockSubheader from 'components/typography/text-block-subheader';
 
 import styles from './styles.scss';
 
+class PortfolioImage extends Component {
+  static propTypes = {
+    portfolio: PropTypes.shape({}),
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovering: false,
+    };
+  }
+
+  handleMouseEnter = () => this.setState({ hovering: true });
+
+  handleMouseExit = () => this.setState({ hovering: false });
+
+  render() {
+    const { portfolio } = this.props;
+    const { hovering } = this.state;
+
+    return (
+      <Col xs={6} lg={4} className={styles['margin-bottom']}>
+        <a
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseExit}
+          className={styles['portfolio-link']}
+          href={portfolio.url}
+        >
+          <img
+            className={styles['portfolio-logos']}
+            src={portfolio.source}
+            role={portfolio.alt}
+          />
+          <div
+            className={cx(
+              styles['text-wrapper'],
+              { [styles['hide-text-wrapper']]: hovering },
+            )}
+          >
+            <TextBlockHeader>{portfolio.title}</TextBlockHeader>
+            <TextBlockSubheader>
+              {portfolio.description}
+            </TextBlockSubheader>
+          </div>
+        </a>
+      </Col>
+    );
+  }
+}
+
 const Portfolio = () =>
   <Scroll.Element name={sections[3].name} className={styles.base}>
     <Row>
       <Col
         xs={12}
-        lg={10}
-        lgOffset={1}
+        lg={8}
+        lgOffset={2}
       >
-        <Row>
-          <Col xs={12}>
-            <SectionHeader>Portfolio</SectionHeader>
-          </Col>
-        </Row>
         <Row className={styles['content-wrapper']}>
           {portfolioImages.map((portfolio, index) =>
-            <Col key={index} xs={6} lg={4}>
-              <a className={styles['portfolio-link']} href={portfolio.url}>
-                <img
-                  className={styles['portfolio-logos']}
-                  src={portfolio.source}
-                  role={portfolio.alt}
-                />
-              </a>
-              <div className={styles['text-wrapper']}>
-                <TextBlockHeader>{portfolio.title}</TextBlockHeader>
-                <TextBlockSubheader>
-                  {portfolio.description}
-                </TextBlockSubheader>
-              </div>
-            </Col>,
+            <PortfolioImage portfolio={portfolio} key={index} />,
           )}
         </Row>
       </Col>
