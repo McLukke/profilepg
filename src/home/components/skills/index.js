f5d91b59630e811ad840f5b7a1180243d882b896
Kenny Lu <Kenny@whynotkenny.com>
2017-03-11 16:34:24 -0500

    Add grayscale background, apply new colours to skills panel

--- src/home/components/skills/index.js
+++ src/home/components/skills/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Scroll from 'react-scroll';
 import { mySkills, sections } from 'content';
-import variables from 'variables';
 import { Circle } from 'rc-progress';
+import carBg from 'images/backgrounds/car_bg.jpg';
 
 import styles from './styles.scss';
 
 const Skills = () =>
   <Scroll.Element name={sections[2].name} className={styles.base}>
-    {mySkills.map((skill, index) =>
-      <div key={index} className={styles['progress-bar-wrapper']}>
-        <Circle
-          className={styles['progress-bar']}
-          strokeWidth="12"
-          trailWidth="12"
-          percent={skill.percentage}
-          strokeLinecap="square"
-          strokeColor={variables.aqua}
-        />
-        <span className={styles['skill-title']}>
-          {skill.name}
-        </span>
-      </div>,
-    )}
+    <div className={styles['image-container']}>
+      <img src={carBg} alt="" className={styles['car-bg']} />
+    </div>
+    <div className={styles['content-wrapper']}>
+      {mySkills.map((skill, index) =>
+        <div key={index} className={styles['progress-bar-wrapper']}>
+          <Circle
+            strokeWidth="12"
+            trailWidth="12"
+            percent={skill.percentage}
+            strokeLinecap="square"
+            trailColor="#373737"
+            strokeColor="#ff4646"
+          />
+          <span className={styles['skill-title']}>
+            {skill.name}
+          </span>
+        </div>,
+      )}
+    </div>
   </Scroll.Element>;
 
 export default Skills;
