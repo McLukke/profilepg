6999bd0a69683afa9bc5478aca893fb4fac63fd7
Kenny Lu <Kenny@whynotkenny.com>
2017-04-14 13:44:36 -0400

    Linting / begin rework of Work Exp section

--- src/home/components/work/index.js
+++ src/home/components/work/index.js
@@ -20,37 +20,22 @@ const Work = () =>
         lgOffset={2}
       >
         <Row className={styles['section-wrapper']}>
-          <Col xs={12} lg={2}>
-            <SectionHeader bottomMargin>Work Experience</SectionHeader>
+          <Col xs={12} lg={4}>
+            <div className={styles['panel']}>
+              content 1
+            </div>
           </Col>
 
-          <Col xs={12} lg={10}>
-            {myWorkExp.map(workExp =>
-              <Row key={workExp.id} className={styles['section-entry']}>
-                <Col xs={9}>
-                  <TextBlockHeader red>{workExp.header}</TextBlockHeader>
-                  <TextBlockSubheader bottomMargin>{workExp.subheader}</TextBlockSubheader>
-                  <div>
-                    {workExp.description.constructor === Array ?
-                      workExp.description.map((descript, index) =>
-                        <div key={`${descript.title}-${index}`}>
-                          <div>{descript.title}</div>
-                          {descript.content.map((workContent, i) =>
-                            <div key={`${workExp.subheader}-${i}`}>-{workContent}</div>,
-                          )}
-                          <br />
-                        </div>,
-                      )
-                    : workExp.description}
-                  </div>
-                </Col>
+          <Col xs={12} lg={4}>
+            <div className={styles['panel']}>
+              content 2
+            </div>
+          </Col>
 
-                <Col xs={3}>
-                  <TextBlockHeader>{workExp.yearFrom} - {workExp.yearTo}</TextBlockHeader>
-                  <p>{workExp.location}</p>
-                </Col>
-              </Row>,
-            )}
+          <Col xs={12} lg={4}>
+            <div className={styles['panel']}>
+              content 3
+            </div>
           </Col>
         </Row>
       </Col>
@@ -58,3 +43,30 @@ const Work = () =>
   </Scroll.Element>;
 
 export default Work;
+
+// {myWorkExp.map(workExp =>
+//   <Row key={workExp.id} className={styles['section-entry']}>
+//     <Col xs={9}>
+//       <TextBlockHeader red>{workExp.header}</TextBlockHeader>
+//       <TextBlockSubheader bottomMargin>{workExp.subheader}</TextBlockSubheader>
+//       <div>
+//         {workExp.description.constructor === Array ?
+//           workExp.description.map((descript, index) =>
+//             <div key={`${descript.title}-${index}`}>
+//               <div>{descript.title}</div>
+//               {descript.content.map((workContent, i) =>
+//                 <div key={`${workExp.subheader}-${i}`}>-{workContent}</div>,
+//               )}
+//               <br />
+//             </div>,
+//           )
+//         : workExp.description}
+//       </div>
+//     </Col>
+//
+//     <Col xs={3}>
+//       <TextBlockHeader>{workExp.yearFrom} - {workExp.yearTo}</TextBlockHeader>
+//       <p>{workExp.location}</p>
+//     </Col>
+//   </Row>,
+// )}
