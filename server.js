6f24e844bc29b957950ab6e992bb0a10c2c304dc
Kenny Lu <Kenny@whynotkenny.com>
2017-04-15 17:00:44 -0400

    Revert "update libs"
    This reverts commit 792d7117aaa69eb4ad3c2c8eea755bfa632dcb32.

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
+var path = require('path');
+var webpack = require('webpack');
+var express = require('express');
+var config = require('./webpack.config');
 
-var config = require('./webpack.config')({ dev: true });
+var app = express();
+var compiler = webpack(config);
 
-config.output.publicPath = '/';
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  stats: 'errors-only'
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const PORT = 3000;
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-}).listen(PORT, '0.0.0.0', (err) => {
+app.listen(PORT, function(err) {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening at http://localhost:${PORT}/`);
+    return console.error(err);
   }
-});
+
+  console.log(`Listening at http://localhost:${PORT}/`);
+})
