2837407ffa8810ff6c840b47ac3fc6619ff893c6
Kenny Lu <Kenny@whynotkenny.com>
2017-04-15 12:09:11 -0400

    Display new images

--- src/home/components/work/index.js
+++ src/home/components/work/index.js
@@ -3,8 +3,8 @@ import Scroll from 'react-scroll';
 import { sections, myWorkExp } from 'content';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import SectionHeader from 'components/typography/section-header';
 import TextBlockHeader from 'components/typography/text-block-header';
+import TextBlock from 'components/typography/text-block';
 
 import styles from './styles.scss';
 
@@ -13,16 +13,17 @@ const Work = () =>
     <Row>
       <Col
         xs={12}
-        md={10}
-        mdOffset={1}
         lg={8}
         lgOffset={2}
       >
         <Row className={styles['section-wrapper']}>
           {myWorkExp.map(exp =>
             <Col xs={12} lg={4} key={exp.id}>
-              <SectionHeader>{exp.title}</SectionHeader>
-              <TextBlockHeader>{exp.description}</TextBlockHeader>
+              <div className={styles.content}>
+                <img src={exp.svg} alt={exp.title} />
+                <TextBlockHeader>{exp.title}</TextBlockHeader>
+                <TextBlock>{exp.description}</TextBlock>
+              </div>
             </Col>,
           )}
         </Row>
