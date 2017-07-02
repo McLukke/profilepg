686abcc4339601277974ed85b06a84d39c33f0ce
Kenny Lu <kenny@whynotkenny.com>
2017-07-01 20:40:48 -0400

    setup state for selective image rendering

--- src/home/components/landing-image/index.js
+++ src/home/components/landing-image/index.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Scroll from 'react-scroll';
 import { sections, homeContent } from 'content';
 import kennyBg from 'images/backgrounds/kenny_bg.jpg';
+// import kennyPortrait from 'images/backgrounds/kenny_bg2.jpg';
 import SectionHeader from 'components/typography/section-header';
 import HighlightText from 'components/typography/highlight-text';
 
 import styles from './styles.scss';
 
-const LandingImage = () =>
-  <Scroll.Element name={sections[0].name} className={styles.base}>
-    <div className={styles['image-container']}>
-      <img
-        src={kennyBg}
-        role="presentation"
-        className={styles['background-image']}
-      />
-      <div className={styles['text-wrapper']}>
-        <SectionHeader upsize red>{homeContent.title}</SectionHeader>
+export default class LandingImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+    };
+  }
 
-        <HighlightText italic>{homeContent.aboutMe}</HighlightText>
-      </div>
-    </div>
-  </Scroll.Element>;
+  componentWillMount() {
+    this.updateDimensions();
+  }
 
-export default LandingImage;
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    const w = window;
+    const d = document;
+    const documentElement = d.documentElement;
+    const body = d.getElementsByTagName('body')[0];
+    const width = w.innerWidth || documentElement.clientWidth || body.clientWidth;
+
+    this.setState({ width });
+  }
+
+  render() {
+    const { width } = this.state;
+
+    return (
+      <Scroll.Element name={sections[0].name} className={styles.base}>
+        <div className={styles['image-container']}>
+          <img
+            src={width >= 768 ? kennyBg : kennyBg}
+            role="presentation"
+            className={styles['background-image']}
+          />
+          <div className={styles['text-wrapper']}>
+            <SectionHeader upsize red>{homeContent.title}</SectionHeader>
+
+            <HighlightText italic>{homeContent.aboutMe}</HighlightText>
+          </div>
+        </div>
+      </Scroll.Element>
+    );
+  }
+}
